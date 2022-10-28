b2cf8b7320815aa4eb1951c06b4583fee462572e
Candela Marin <candelaemarin@outlook.com>
2022-10-27 23:55:05 -0300

    ejercicios y test de gano y perdio

--- ejercicios.cpp
+++ ejercicios.cpp
@@ -37,12 +37,24 @@ void cambiarBanderita(tablero& t, jugadas& j, pos p, banderitas& b) {
 
 /******++++**************************** EJERCICIO perdio ***********+++***********************/
 bool perdio(tablero& t, jugadas& j) {
-    // ...
+    bool perdio = false;
+    for (int i=0; i < j.size(); i++) {
+        int p1 = (j[i].first).first;
+        int p2 = (j[i].first).second;
+        if (t[p1][p2]) perdio = true;
+    } return perdio;
 }
 
 /******++++**************************** EJERCICIO gano ***********+++***********************/
 bool gano(tablero& t, jugadas& j) {
-    // ...
+    int posSinMinas =0;
+    bool gano = false;
+    if (!perdio(t,j)) {
+    for (int i=0; i < t.size(); i++)
+        for (int r=0; r < t[0].size(); r++)
+        if (!t[i][r]) posSinMinas++;
+        if (posSinMinas == j.size()) gano = true; // uso j.size porque todas las jugadas son validas  y no tienen minas
+        } return gano;
 }
 
 /******++++**************************** EJERCICIO jugarPlus ***********+++***********************/
--- tests/perdioTEST.cpp
+++ tests/perdioTEST.cpp
@@ -6,3 +6,33 @@
 #include "gtest/gtest.h"
 
 using namespace std;
+
+TEST(perdioTEST, noPerdio){
+    vector<bool> v1 = {true, false, false};
+    vector<bool> v2 = {true, false, false};
+    vector<bool> v3 = {true, true, true};
+    tablero t = {v1, v2, v3};
+
+    pos p1 = {1,2};
+    pos p2 = {0,1};
+    pos p3 = {0,2};
+
+    jugadas j = {{p1, minasAdyacentes(t,p1)}, {p2, minasAdyacentes(t,p2)}, {p3, minasAdyacentes(t,p3)}};
+
+    EXPECT_FALSE(perdio(t, j));
+}
+
+TEST(perdioTEST, perdio){
+    vector<bool> v1 = {true, false, true};
+    vector<bool> v2 = {true, false, false};
+    vector<bool> v3 = {true, true, true};
+    tablero t = {v1, v2, v3};
+
+    pos p1 = {1,2};
+    pos p2 = {0,1};
+    pos p3 = {0,2};
+
+    jugadas j = {{p1, minasAdyacentes(t,p1)}, {p2, minasAdyacentes(t,p2)}, {p3, minasAdyacentes(t,p3)}};
+
+    EXPECT_TRUE(perdio(t, j));
+}
